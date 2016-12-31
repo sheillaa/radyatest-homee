f42f1f442261a2a051bba4a9261a9bb63f8a9840
Sheilla Adila <sheilladila@gmail.com>
2017-01-01 05:22:04 +0700

    Default Instalation

---
+++ Homee/Homee/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Homee.MvcApplication" Language="C#" %>
---
+++ Homee/Homee/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Homee.Startup))]
+namespace Homee
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
